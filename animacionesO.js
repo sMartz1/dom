659ccaaa71a294277005c3be23b0a6d3022c83f2
SebMart <ra3gan1991@gmail.com>
2019-08-29 15:33:58 +0200

    Valores por defecto

--- animacionesO.js
+++ animacionesO.js
@@ -1,10 +1,13 @@
 jQuery(document).ready(function($) {
+	var botonActual;
 	$(".bton").click(function() {
-		console.log("btnTrigger");
+		
 		setupModal(this);
 		$("#myModal").modal();
 	});
+
 	$(".iG").on("change paste keyup", function() {
+		console.log("Se ha cambiado valor");
      	$(".assBtn").empty();
 		$(".assBtn").append($(this).val());
 	});
@@ -12,9 +15,22 @@ jQuery(document).ready(function($) {
 
 function setupModal(x){
 	console.log(x);
+	botonActual = $(x).attr("id");
+	console.log("Valor de boton actual : " + botonActual);
 	buttonToModify = $(x).find(".btnTitle").text();
 	$(".iG").attr("placeholder", buttonToModify);
 	console.log(buttonToModify);
 	$(".assBtn").empty();
 	$(".assBtn").append(buttonToModify);
+	$("#saveChanges").click(function(){
+		var valToSave = $(".iG").val();
+		var save = {};
+		save[botonActual] = valToSave;
+		
+		chrome.storage.sync.set(save);		    
+		chrome.storage.sync.get([botonActual],function(x){
+			console.log(x[botonActual]);
+		});
+		
+	});
 }
