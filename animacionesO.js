66001d40aef3c861fc0d3281cb45aafdc17d0a4a
SebCasa <ra3gan1991@gmail.com>
2019-08-31 09:06:10 +0200

    Se implementa input para grupo en modal y se link datos con storage

--- animacionesO.js
+++ animacionesO.js
@@ -7,10 +7,15 @@ jQuery(document).ready(function($) {
 	});
 
 	$(".iG").on("change paste keyup", function() {
+		checkInputs();
 		console.log("Se ha cambiado valor");
 		checkIg(this);
      	$(".assBtn").empty();
 		$(".assBtn").append($(this).val());
+
+	});
+		$(".iGp").on("change paste keyup", function() {
+		checkInputs();
 
 	});
 });
@@ -30,9 +35,12 @@ function setupModal(x){
 			alert("No puedes dejar vacia la cabecera del boton...pichon...");
 			
         }else{
-		var valToSave = $(".iG").val();
+		var valToSaveIg = $(".iG").val();
+		var valToSaveIgp = $(".iGp").val();
 		var save = {};
-		save[botonActual] = valToSave;
+		bGp = botonActual +"G";
+		save[botonActual] = valToSaveIg;
+		save[bGp]= valToSaveIgp;
 		
 		
 		chrome.storage.sync.set(save);		    
@@ -40,6 +48,7 @@ function setupModal(x){
 			console.log(x[botonActual]);
 			$(".iG").val("");
 			$(".assBtn").empty();
+			$(".iGp").val("");
 			$("#saveChanges").removeAttr("data-dismiss");
 		});
 	}	
@@ -52,4 +61,25 @@ function checkIg(x){
 			console.log("Se añade clase a boton");
 			$("#saveChanges").attr("data-dismiss","modal");
 		}
+}
+
+function checkInputs(){
+	if(!$(".iG").val()){
+			console.log("Ig Vacio");
+			$(".iG").css("background","#c72929");
+			
+        }else{
+        	console.log("Ig Valor");
+        	$(".iG").css("background","white");
+
+        }
+    if(!$(".iGp").val()){
+			console.log("Ig Vacio");
+			$(".iGp").css("background","#c72929");
+			
+        }else{
+        	console.log("Ig Valor");
+        	$(".iGp").css("background","white");
+
+        }
 }
